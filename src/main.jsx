35c45637ccb65cf3e3ad76bbad340959a77b5f1f
Dylan Farkas <dylan.farkas@correounivalle.edu.co>
2025-04-28 15:57:58 -0500

    Branch develop and router created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import RoutesProject from './router/RoutesProject.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RoutesProject />
   </StrictMode>,
 )
